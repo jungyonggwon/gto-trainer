9f5cfa3fb403f0cde9bbf4a3a5c83742732324a9
jungyonggwon <wjddydrnjs34@gmail.com>
2025-03-16 21:46:38 +0900

    Tailwind CSS 설정 완료 및 프로젝트 기본 설정

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // ← JSX, TSX도 감지하도록 설정
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
